cee93cef7b423154309331c6a5e34137e591f87f
Fırat Çelik <49812357+Fcelikq@users.noreply.github.com>
2020-01-04 19:09:59 +0300

    Lastv1

--- js/shop.js
+++ js/shop.js
@@ -96,53 +96,11 @@ jQuery(document).ready(function($) {
 	});
 	
 	////////////////////////////////////home-slider
-	$(function() {
-		$("#srch").click(function() {
-		  $('.srchwrap').toggle('fast', function() {
-		  });
-		});
-	});
-	$(function() {
-		$("#reg").click(function() {
-		  $('.regwrap').toggle('fast', function() {
-		  });
-		});
-	});
-	$(function() {
-		$("#popcart").click(function() {
-		  $('.popcart').toggle('fast', function() {
-		  });
-		});
-	});
+
 	
 	////////////////////////////////////map
-	$(function() { 
-		demo.add(function() {
-			$('#map_canvas').gmap({'center': '57.7973333,12.0502107', 'zoom': 10, 'disableDefaultUI':true, 'callback': function() {
-				var self = this;
-				self.addMarker({'position': this.get('map').getCenter() }).click(function() {
-				self.openInfoWindow({ 'content': 'Hello World!' }, this);
-				});	
-			}});
-		}).load();
-	});
-	
-	$(function() {
-		$('#myTab a').click(function (e) {
-		  e.preventDefault()
-		  $(this).tab('show')
-		});
-	});
 	
 	
-	$(function() {
-		$('.fancybox').fancybox();
-	});
 	
-	$(function() {
-		$.fn.raty.defaults.path = 'js/rate/images';
-		$('#default').raty();
-		$('#score').raty({ readOnly: true, score: 3 });
-	});
-
+	
 });
